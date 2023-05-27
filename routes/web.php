60d4c886a1f079a3c2d74aa13409e86fcde902a5
LuizEddie <contato.luiz1997@gmail.com>
2023-05-26 23:09:30 -0300

    Luiz: Aula 39

--- routes/web.php
+++ routes/web.php
@@ -22,26 +22,28 @@ use Illuminate\Support\Facades\Route;
 //     return 'Olá, seja bem-vindo ao curso';
 // });
 
-Route::get("/", 'PrincipalController@principal');
+Route::get("/", 'PrincipalController@principal')->name('site.index');
 
-Route::get("/contato", "ContatoController@contato");
+Route::get("/contato", "ContatoController@contato")->name('site.contato');
 
-Route::get("/sobre-nos", "SobreNosController@sobreNos");
+Route::get("/sobre-nos", "SobreNosController@sobreNos")->name('site.sobrenos');
 
 Route::get("/login", function(){
     return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get("/clientes", function(){
+        return 'Clientes';
+    })->name('app.clientes');
+    Route::get("/fornecedores", function(){
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+    Route::get("/produtos", function(){
+        return 'Produtos';
+    })->name('app.produtos');
 });
 
-
-Route::get("/clientes", function(){
-    return 'Clientes';
-});
-Route::get("/fornecedores", function(){
-    return 'Fornecedores';
-});
-Route::get("/produtos", function(){
-    return 'Produtos';
-});
 
 // Route::get("/contato/{nome}/{categoria_id}", function(string $nome = "Sem nome", int $categoria_id = 1){
 //     echo "Nome: ".$nome."<br>";
