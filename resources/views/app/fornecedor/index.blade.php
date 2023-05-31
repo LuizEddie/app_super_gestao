c954d47a53566e57c909fd0288147851c76ca127
LuizEddie <contato.luiz1997@gmail.com>
2023-05-31 20:01:54 -0300

    Luiz: Aula 45

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,10 @@
+<h3>Fornecedor</h3>
+<?= 'Teste' ?>
+{{'Teste'}}
+{{-- Fica o comentario que será descartado pelo interpretador do blade --}}
+
+@php
+ //Comentario feito usando a sintaxe nativa do php
+ echo 'salve'
+
+@endphp
