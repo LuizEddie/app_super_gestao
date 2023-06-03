22d32fedc2b60c153b8bed72620e89006e9795e4
LuizEddie <contato.luiz1997@gmail.com>
2023-06-03 11:22:33 -0300

    Aula 59: variavel loop

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -10,10 +10,11 @@
 
 @isset($fornecedores)
     @forelse($fornecedores as $f)
-        Fornecedor: @{{ $f['nome']}}<br>
-        Status: @{{ $f['status']}}<br>
-        CNPJ: @{{ $f['cnpj'] ?? 'Dado não preenchido'}}<br>
-        Telefone: @{{ $f['ddd'] ?? ''}} @{{ $f['telefone'] ?? ''}}<br>
+        Iteração atual: {{ $loop->iteration}}<br>
+        Fornecedor: {{ $f['nome']}}<br>
+        Status: {{ $f['status']}}<br>
+        CNPJ: {{ $f['cnpj'] ?? 'Dado não preenchido'}}<br>
+        Telefone: {{ $f['ddd'] ?? ''}} {{ $f['telefone'] ?? ''}}<br>
         @switch($f['ddd'])
             @case('11')
                 São Paulo - SP
@@ -27,6 +28,16 @@
             @default
                 Estado não identificado
         @endswitch
+        <br>
+        @if($loop->first) 
+            Primeira iteração do loop
+        @endif
+
+        @if($loop->last)
+            Ultima iteração do loop
+            <br>
+            Total de Registros: {{ $loop->count}}
+        @endif
         <hr>
     @empty
         Não existem fornecedores cadastrados
