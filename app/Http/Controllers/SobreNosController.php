aa22f70c52b206b273458f54c5fd7265184e586c
LuizEddie <contato.luiz1997@gmail.com>
2023-06-05 19:46:45 -0300

    Aula 65: Template

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class SobreNosController extends Controller
 {
     public function sobreNos(){
-        return view('site.sobre-nos');
+        $titulo = 'Sobre Nós';
+
+        return view('site.sobre-nos', compact('titulo'));
     }
 }
