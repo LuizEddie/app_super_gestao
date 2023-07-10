505d3fa7dfe4d6315efa56e66d82b21645ca1b66
LuizEddie <contato.luiz1997@gmail.com>
2023-07-10 20:10:42 -0300

    Aula 141 Adicionando middleware a todas as rotas de um grupo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +17,10 @@ use Illuminate\Support\Facades\Route;
     Verbos http
     get, post, put, delete, patch
 */
-Route::middleware(LogAcessoMiddleware::class)->get("/", "PrincipalController@principal")->name("site.index");
+Route::get("/", "PrincipalController@principal")->name("site.index");
 // Route::get("/", 'PrincipalController@principal')->name('site.index')->middleware(LogAcessoMiddleware::class);
 
-Route::middleware(LogAcessoMiddleware::class)->get("/contato", "ContatoController@contato")->name('site.contato');
+Route::get("/contato", "ContatoController@contato")->name('site.contato');
 Route::post("/contato", "ContatoController@salvar")->name('site.contato');
 
 Route::get("/sobre-nos", "SobreNosController@sobreNos")->name('site.sobrenos');
